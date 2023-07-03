d22f8e388a6c3f4357d8426e21e1f03e11e4f67c
Matthew Rule <matt.rule.github@gmail.com>
2023-07-03 18:49:00 +0100

    Add view functions

--- wwwroot/ts/view.ts
+++ wwwroot/ts/view.ts
@@ -7,34 +7,50 @@ export class View
     public unzoomedCellWidth: number = 20;
     public cellWidth: number = 20;
     
-    private dynamicGridPosition: Vec = new Vec(0, 0);       // Changes all the time when panning the view
-    private commitGridPosition: Vec = new Vec(0, 0);        // Does not change until finished panning the view
+    private dynamicViewPosition: Vec = new Vec(0, 0);       // Changes all the time when panning the view
+    private commitViewPosition: Vec = new Vec(0, 0);        // Does not change until finished panning the view
     public startDragScreenPosition: Vec | null = null;
 
     public screenToCellCoords(screenXY: Vec): Vec
     {
         // perform transformation from screen to cell coordinates, considering panning and zoomed grid cell width
-        return screenXY.subtract(this.dynamicGridPosition).divide(this.cellWidth).floor();
+        return screenXY.subtract(this.getDynamicGridPosition()).divide(this.cellWidth).floor();
     }
     public cellToScreenCoords(cellXY: Vec): Vec
     {
         // perform transformation from cell to screen coordinates, considering panning and zoomed grid cell width
-        return cellXY.multiply(this.cellWidth).add(this.dynamicGridPosition);
+        return cellXY.multiply(this.cellWidth).add(this.getDynamicGridPosition());
     }
     public getDynamicGridPosition(): Vec
     {
-        return this.dynamicGridPosition.clone();
+        return this.dynamicViewPosition.negative();
     }
     public setDynamicGridPosition(val: Vec): void
     {
-        this.dynamicGridPosition = val.clone();
+        this.dynamicViewPosition = val.negative();
     }
     public getCommitGridPosition(): Vec
     {
-        return this.dynamicGridPosition.clone();
+        return this.dynamicViewPosition.negative();
     }
     public setCommitGridPosition(val: Vec): void
     {
-        this.dynamicGridPosition = val.clone();
+        this.dynamicViewPosition = val.negative();
+    }
+    public getDynamicViewPosition(): Vec
+    {
+        return this.dynamicViewPosition.clone();
+    }
+    public setDynamicViewPosition(val: Vec): void
+    {
+        this.dynamicViewPosition = val.clone();
+    }
+    public getCommitViewPosition(): Vec
+    {
+        return this.dynamicViewPosition.clone();
+    }
+    public setCommitViewPosition(val: Vec): void
+    {
+        this.dynamicViewPosition = val.clone();
     }
 }
